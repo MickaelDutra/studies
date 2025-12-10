10d326118101cf9723fb6d79241c40fb8e2892df
mickael_dutra <mickaeldutra99@gmail.com>
2025-12-10 19:51:15 -0300

    Addicionando filmografia

--- projeto_tarantino/build/src/script/main.js
+++ projeto_tarantino/build/src/script/main.js
@@ -1,55 +1,38 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const modalOverlay = document.getElementById('modal-overlay');
-//     const openLinks = document.querySelectorAll('.open-modal-link'); // Pega todos os links
-//     const modalTitle = document.getElementById('modal-title');
-//     const modalBody = document.getElementById('modal-body');
-//     const modalPoster = document.getElementById('modal-poster');
+document.addEventListener('DOMContentLoaded', () => {
+    const filmItens = document.querySelectorAll(".open-modal-link");
 
-//     openLinks.forEach(link => {
-//         link.addEventListener('click', function (event) {
-//             event.preventDefault();
+    filmItens.forEach(item => {
+        item.addEventListener('click', function () {
+            openModel(this);
+        })
+    })
 
-//             // 1. Pega o ID do filme (do elemento pai do link, ou do próprio link)
-//             const itemId = link.closest('.filmography__item').dataset.filmeId;
-//             const data = filmeData[itemId];
+    const modalOverlay = document.querySelector(".modal-overlay");
+    modalOverlay.addEventListener('click', closeModel);
+})
 
-//             if (data) {
-//                 // 2. Preenche os elementos do modal com os dados
-//                 modalTitle.textContent = data.titulo;
-//                 modalBody.innerHTML = `<p>${data.sinopse}</p>`;
-//                 modalPoster.src = data.poster;
 
-//                 // 3. Abre o modal
-//                 modalOverlay.classList.add('--is-active');
-//             }
-//         });
-//     });
+function openModel(clickedElement) {
+    const tittle = clickedElement.getAttribute("data-tittle");
+    const synopsis = clickedElement.getAttribute("data-synopsis");
+    const imgSrc = clickedElement.getAttribute("data-img-src");
+    console.log(imgSrc);
 
-//     function openModal(event) {
-//         event.preventDefault(); // Evita que o link # mude a URL
-//         modalOverlay.classList.add('active');
-//     }
+    const modalTitleEl = document.getElementById("modal-title");
+    const modalBodyEl = document.getElementById("modal-body");
+    const modalImageEl = document.getElementById("modal-image");
+    const modalOverlayEl = document.querySelector(".modal-overlay");
 
-//     // Função para fechar o modal
-//     function closeModal() {
-//         modalOverlay.classList.remove('--is-active');
-//     }
+    modalTitleEl.textContent = tittle;
+    modalBodyEl.textContent = synopsis;
+    modalImageEl.src = imgSrc;
+    modalImageEl.alt = tittle;
 
-//     // 1. ABRIR ao clicar no link
-//     openLinks.addEventListener('click', openModal);
+    modalOverlayEl.style.display = "block";
+}
 
-//     // 2. FECHAR ao clicar no botão X (opcional)
-//     if (closeBtn) {
-//         closeBtn.addEventListener('click', closeModal);
-//     }
 
-//     // 3. FECHAR ao clicar FORA do pop-up (O SEGREDO)
-//     modalOverlay.addEventListener('click', function (event) {
-//         // Se o clique for DIRETAMENTE no overlay (e não em um filho do modal-content)
-//         // usamos o !event.target.closest para verificar se o elemento clicado (target)
-//         // está DENTRO do modal-content.
-//         if (!event.target.closest('.modal-content')) {
-//             closeModal();
-//         }
-//     });
-// });
+function closeModel() {
+    const overlay = document.getElementById("model");
+    overlay.style.display = "none";
+}
