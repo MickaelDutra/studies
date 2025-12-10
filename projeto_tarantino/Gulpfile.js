06bb065f8db62e2e5812414f0cea70099786828a
mickael_dutra <mickaeldutra99@gmail.com>
2025-12-10 00:02:38 -0300

    commit inicial - build do projeto

---
+++ projeto_tarantino/Gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+// const sass = require('gulp-sass')(require('sass'));
+// const uglify = require('gulp-uglify');
+// const imagemin = require('gulp-imagemin');
+
+
+
+// function buildImage() {
+//     return gulp.src('build/src/assets/image/**/*.{jpg,png,jpeg,gif,svg}')
+//         .pipe(imagemin([
+//             imagemin.gifsicle({ interlaced: true }),
+//             imagemin.mozjpeg({ quality: 75, progressive: true }),
+//             imagemin.optipng({ optimizationLevel: 5 }),
+//             imagemin.svgo({
+//                 plugins: [
+//                     { removeViewBox: true },
+//                     { cleanupIDs: false }
+//                 ]
+//             })
+//         ]))
+//         .pipe(gulp.dest('dist/assets/image'))
+// };
+
+// exports.default = buildImage;
