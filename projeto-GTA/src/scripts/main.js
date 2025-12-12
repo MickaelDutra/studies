15fa0b06982fe514d30736d298b3885982fb7328
mickael_dutra <mickaeldutra99@gmail.com>
2025-12-12 08:33:37 -0300

    adicionando projeto

---
+++ projeto-GTA/src/scripts/main.js
@@ -0,0 +1,30 @@
+AOS.init();
+
+
+const date = new Date("nov 19, 2026 00:00:00");
+const timeStampDate = date.getTime();
+
+const countdown = setInterval(() => {
+    const now = new Date;
+    const timeStampDateNow = now.getTime();
+
+    const timeToEvent = timeStampDate - timeStampDateNow;
+
+    const dayMs = 1000 * 60 * 60 * 24;
+    const hourMS = 1000 * 60 * 60;
+    const minMS = 1000 * 60;
+
+
+    const dayToEvent = Math.floor(timeToEvent / dayMs);
+    const hourToEvent = Math.floor((timeToEvent % dayMs) / hourMS);
+    const minToEnvent = Math.floor((timeToEvent % hourMS) / minMS);
+    const secToEnvent = Math.floor((timeToEvent % minMS) / 1000);
+
+    document.getElementById('date').innerHTML =  (`será lançado em: ${dayToEvent}d ${hourToEvent}h ${minToEnvent}m ${secToEnvent}s`)
+
+    if(timeToEvent < 0){
+        clearInterval(countdown);
+
+         document.getElementById('date').innerHTML =  ('já está disponível!!! 😎🤪')
+    }
+}, 1000);
